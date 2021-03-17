f8124952c9284297d356c68b5224ab08c6bb9224
mehandru-dev <72070505+mehandru-dev@users.noreply.github.com>
2021-03-17 22:40:59 +0530

    Add files via upload

---
+++ js/mango.js
@@ -0,0 +1,42 @@
+class mango{
+    constructor(x, y,width,height) {
+        var options = {
+           isStatic:false,
+            restitution :0.4,
+            friction :0.2
+			
+        }
+		this.Visiblity=255;
+        this.body = Bodies.rectangle(x, y, width,height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+    
+		 if(this.body.speed<=7){
+		// var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x, pos.y);
+       // rotate(angle);
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
+        pop();
+		  }
+	  
+		  else {
+			var pos= this.body.position;
+			World.remove(world,this.body);
+			push();
+			this.Visiblity=this.Visiblity-1;
+			translate(pos.x, pos.y);
+			tint(255,this.Visiblity);
+			//rectMode(CENTER);
+			rect(this.body.position.x,this.body.position.y,this.width, this.height);
+			pop();
+		  }
+
+
+      }
+}
